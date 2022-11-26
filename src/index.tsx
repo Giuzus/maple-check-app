9051250723041389723c946d59c5cbcc86297adb
Gui Rangel <guirangel.310@gmail.com>
2022-11-26 16:56:08 -0300

    Add bossing and leveling checklists

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
