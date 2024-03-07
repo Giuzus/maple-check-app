8c5b9b239e4bf3974760362f9eedca3c2a4aa9d3
Gui Rangel <guirangel.310@gmail.com>
2024-03-07 17:45:48 -0300

    Fix router basename

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,12 @@ import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/home/home.page.tsx";
 
-
-const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-]);
+const router = createBrowserRouter([{ path: "/", element: <Home /> }], {
+  basename: "/vite-react-boilerplate",
+});
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
