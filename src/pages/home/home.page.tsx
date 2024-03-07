8252e0b69119abe28e2e6a230db2850e37c1bd89
Gui Rangel <guirangel.310@gmail.com>
2024-03-07 17:29:08 -0300

    Lint fix

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,9 +1,3 @@
-import clsx from "clsx";
-import { useRef } from "react";
-import { Link } from "react-router-dom";
-
 export default function Home() {
-  return (
-    <h1>/home</h1>
-  );
+  return <h1>/home</h1>;
 }
